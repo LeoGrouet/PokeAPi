1feb63316f603bdd9ef86ce89b31636e81a064c3
LeoGrouet <leo.grouet@gmail.com>
2025-02-07 13:55:28 +0100

    chore : redwood init

---
+++ app/api/jest.config.js
@@ -0,0 +1,6 @@
+const config = {
+  rootDir: '../',
+  preset: '@redwoodjs/testing/config/jest/api',
+}
+
+module.exports = config
---
+++ app/graphql.config.js
@@ -0,0 +1,9 @@
+const { getPaths } = require('@redwoodjs/project-config')
+
+/** @type {import('graphql-config').IGraphQLConfig} */
+const config = {
+  schema: getPaths().generated.schema,
+  documents: './web/src/**/!(*.d).{ts,tsx,js,jsx}',
+}
+
+module.exports = config
