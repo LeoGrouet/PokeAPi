848f30d651833c34a04fb52082cb29d5875e30b9
LeoGrouet <leo.grouet@gmail.com>
2025-05-19 21:51:12 +0200

    feat: user entity

---
+++ apps/backend/src/Domain/Model/User.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Model;
+
+use DateTimeImmutable;
+
+class User
+{
+  private string $id;
+  private string $username;
+  private string $email;
+  private string $password;
+  private DateTimeImmutable $createdAt;
+  private DateTimeImmutable $updatedAt;
+
+  public function __construct(
+    string $id,
+    string $username,
+    string $email,
+    string $password,
+    DateTimeImmutable $createdAt,
+    DateTimeImmutable $updatedAt
+  ) {}
+
+  public function getId(): string
+  {
+    return $this->id;
+  }
+
+  public function getUsername(): string
+  {
+    return $this->username;
+  }
+
+  public function getEmail(): string
+  {
+    return $this->email;
+  }
+
+  public function getCreatedAt(): DateTimeImmutable
+  {
+    return $this->createdAt;
+  }
+
+  public function getUpdatedAt(): DateTimeImmutable
+  {
+    return $this->updatedAt;
+  }
+}
